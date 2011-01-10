62c268f7ac3132436deb97f4c5c531ee99270fdc
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-01-10 21:00:26 +0000

    Add a custom AutoRun module to get testing started.

---
+++ test/AutoRun.cpp
@@ -0,0 +1,82 @@
+#include "MethodTimer.h"
+#include "TimeReport.h"
+#include "TestCaseMethodCallers.h"
+#include "PrivateClassContext.h"
+#include "Notification.h"
+#include "ProjectDescription.h"
+#include "ProjectFixture.h"
+#include "FineGrainedMethodDescription.h"
+#include "ClassDescription.h"
+#include "AutoTest.h"
+
+#include "TestResult.h"
+
+#include <sstream>
+#include <iostream>
+
+using namespace Prototype1;
+//using Prototype1::MethodTimer;
+using Prototype1::Internal::MethodCaller;
+//using Prototype1::TimeReport;
+using Prototype1::Internal::ClassContext;
+//using Prototype1::Notification;
+
+
+class MainNotifier: public Notification{
+	const ProjectDescription* project;
+	const ClassDescription* classDesc;
+	const FineGrainedMethodDescription* method;
+	std::stringstream out;
+	virtual void StartProject(const ProjectDescription& description){
+		out << "Start Project: " << description.name() << std::endl;
+		out << "Number of Classes: " << description.numClasses() << std::endl;
+		project=&description;
+	}
+	virtual void StartClass(const ClassDescription& description){
+		//out << " Start Class: " << description.name() << std::endl;
+		classDesc=&description;
+	}
+	virtual void StartFineGrainedMethod(const FineGrainedMethodDescription& description){
+		//out << "  Start Method: " << description.name() << std::endl;
+		method=&description;
+	}
+	virtual void Result(const TestResult& result){
+		//if (!result.result)
+		out << "   Result: " << result.result << std::endl;
+		out << "    Message: " << result.message << std::endl;
+	}
+	virtual void Exception(const std::string& what){
+		out << "   Exception: " << what << std::endl;
+	}
+
+	virtual void EndFineGrainedMethod(){ 
+		out << "  End Method, time:" << method->run_time() << " results:" << method->results() << std::endl; 
+	}
+	virtual void EndClass(){ 
+		out << " End Class, time:" << classDesc->run_time() << " results:" << classDesc->results() << std::endl; 
+	}
+	virtual void EndProject(){ 
+		out << "End Project" <<  std::endl; 
+		out << " results:" << project->results() << std::endl; 
+		out << " runtime:" << project->run_time() << std::endl; 
+		out << " elapsed time:" << project->elapsed_time() << std::endl; 
+	}
+public:
+	void printOutput(std::ostream& output){ output << out.str(); }
+}notifier;
+
+class DefaultMethodTimer: public MethodTimer{
+	void timeMethod(MethodCaller& caller, TimeReport& /* report */){
+		// Only forward the call, we don't do timings yet
+		caller.forward();
+	}
+}timer;
+
+ClassContext context(notifier,timer);
+
+void DoAutoRun(const char* title)
+{
+	ProjectFixture fixture;
+	fixture.run(title,Auto::autoTests(),context);
+	notifier.printOutput(std::cout);
+}
--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+void DoAutoRun(const char* title);
+
 int main(int , char* [])
 {
-	std::cout << "Hello, World!" << std::endl;
+	//std::cout << "Hello, World!" << std::endl;
+	DoAutoRun("Test gppUnit1.5");
 #ifdef WIN32
 	std::cout << "Press return...";
 	std::cin.get();
