43af85354db3c8a8af8213c1c2c6fe9a3576edf8
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-01-11 09:26:15 +0000

    First real failing test case.

--- test/TestTestCase.cpp
+++ test/TestTestCase.cpp
@@ -4,10 +4,44 @@
 
 namespace gppUnit{
 	class TestCase{
-
+	public:
+		virtual void setup(){}
+		virtual void test()=0;
+		virtual void teardown(){}
 	};
 }
+namespace TestTestCase {
+	class MockTestCase: public gppUnit::TestCase{
+		void test(){}
+	public:
+		std::string result(){return "x";}
+	};
+	class Test: public Auto::TestCase{
+		MockTestCase testcase;
 
-class Test: public Auto::TestCase{
-	void test(){ expect.fail("Failing TestCase");}
-}GPPUNIT_INSTANCE;
+		void call(gppUnit::TestCase& testcase){
+			testcase.setup();
+			testcase.test();
+			testcase.teardown();
+		}
+		void givenMockTestCase(){
+		}
+		void whenCalled(){
+			call(testcase);
+		}
+		void thenAllThreeMethodsCalled(){
+			if(testcase.result()=="setup.test.teardown."){
+				confirm.pass(testcase.result().c_str());
+			} else {
+				confirm.fail(testcase.result().c_str());
+			}
+		}
+		void test(){ 
+			//expect.fail("Failing TestCase");
+			givenMockTestCase();
+			whenCalled();
+			thenAllThreeMethodsCalled();
+		}
+	}GPPUNIT_INSTANCE;
+
+}
