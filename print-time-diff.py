c184fee9194ce0b838eca1fb3524687853856085
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-01-12 12:14:46 +0000

    Improved print-time-diff.py

--- print-time-diff.py
+++ print-time-diff.py
@@ -1,7 +1,17 @@
 #print time
 import os
 
+def getSeconds(time):
+    #print time
+    hours=int(time[0:2])
+    mins=int(time[2:4])
+    seconds=int(time[4:6])
+    millis=float(time[7:9])/100
+    return millis+seconds+mins*60+hours*3600
+    
 start=os.getenv("STARTBATCH")
 end=os.getenv("ENDBATCH")
 
-print str(round(float(end)-float(start),3))+" seconds."
+print start
+print end
+print str(round(getSeconds(end)-getSeconds(start),3))+" seconds."
