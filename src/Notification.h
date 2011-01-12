37cb612d7607d014cec5b8610f37ce6b5d25ebaa
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-01-12 11:46:05 +0000

    Added Notification interface.

---
+++ src/Notification.h
@@ -0,0 +1,11 @@
+#ifndef NOTIFICATION_H_FE7AAFFA_316E_4165_9D82_C593A049C539
+#define NOTIFICATION_H_FE7AAFFA_316E_4165_9D82_C593A049C539
+
+namespace gppUnit{
+	class Notification{
+	public:
+		virtual void StartMethod(const std::string& /* name */){}
+	};
+}
+
+#endif // NOTIFICATION_H_FE7AAFFA_316E_4165_9D82_C593A049C539
