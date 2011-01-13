b1c7b2bc178572543a3655c1330bd66f8c8c39e7
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-01-13 12:13:49 +0000

    Added MethodNames.

---
+++ src/MethodNames.h
@@ -0,0 +1,10 @@
+#ifndef METHODNAMES_H_D79FC752_163D_4C0B_B2BC_0387EE3E88E3
+#define METHODNAMES_H_D79FC752_163D_4C0B_B2BC_0387EE3E88E3
+
+namespace gppUnit{
+	const char* setupMethodName();
+	const char* testMethodName();
+	const char* teardownMethodName();
+}
+
+#endif // METHODNAMES_H_D79FC752_163D_4C0B_B2BC_0387EE3E88E3
