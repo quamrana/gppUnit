d9f25add17283d175695406fcbe883c78ca5c123
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-01-25 17:52:31 +0000

    Added equal_to_trait<std::string> and reformatted.

--- src/Matchers.h
+++ src/Matchers.h
@@ -14,21 +14,18 @@ namespace gppUnit {
 		std::string description() const { return strm.toString();}
 	};
 
-	template<typename T, typename U=T>
-	struct ProxyTypeBase
-	{
+	template<typename T, typename U = T>
+	struct ProxyTypeBase {
 		typedef T original_type;
 		typedef U conversion_type;
 
 		conversion_type value;
-		explicit ProxyTypeBase(const original_type& from):value(from){}
+		explicit ProxyTypeBase(const original_type& from): value(from) {}
 
-		static ProxyTypeBase<original_type,conversion_type> create(const original_type& from){ return ProxyTypeBase(from); }
+		static ProxyTypeBase<original_type, conversion_type> create(const original_type& from) { return ProxyTypeBase(from); }
 
-	protected:
 	private:
-		// Don't Use
-		ProxyTypeBase(){}
+		ProxyTypeBase() {}
 	};
 
 	// Define a degenerate derived class
@@ -36,32 +33,28 @@ namespace gppUnit {
 	struct ProxyType: ProxyTypeBase<T> {};
 
 	template<>
-	struct ProxyType<int>: ProxyTypeBase<int,long> {};
+	struct ProxyType<int>: ProxyTypeBase<int, long> {};
 	template<>
-	struct ProxyType<const char*>: ProxyTypeBase<const char*,std::string> {};
+	struct ProxyType<const char*>: ProxyTypeBase<const char*, std::string> {};
 
 	template<typename _OP, typename T, typename U>
-	MatcherResult binary_op(const _OP& op, const T& expected, const U& actual)
-	{
-		return op(ProxyType<T>::create(expected).value,ProxyType<U>::create(actual).value);
+	MatcherResult binary_op(const _OP& op, const T& expected, const U& actual) {
+		return op(ProxyType<T>::create(expected).value, ProxyType<U>::create(actual).value);
 	}
 
 	template<typename _OP, typename T, typename U, size_t size>
-	MatcherResult binary_op(const _OP& op, const T (&expected)[size], const U& actual)
-	{
-		return op(ProxyType<const T*>::create(expected).value,ProxyType<U>::create(actual).value);
+	MatcherResult binary_op(const _OP& op, const T(&expected)[size], const U& actual) {
+		return op(ProxyType<const T*>::create(expected).value, ProxyType<U>::create(actual).value);
 	}
 
 	template<typename _OP, typename T, typename U, size_t size>
-	MatcherResult binary_op(const _OP& op, const T& expected, const U (&actual)[size])
-	{
-		return op(ProxyType<T>::create(expected).value,ProxyType<const U*>::create(actual).value);
+	MatcherResult binary_op(const _OP& op, const T& expected, const U(&actual)[size]) {
+		return op(ProxyType<T>::create(expected).value, ProxyType<const U*>::create(actual).value);
 	}
 
 	template<typename _OP, typename T, typename U, size_t sizeT, size_t sizeU>
-	MatcherResult binary_op(const _OP& op, const T (&expected)[sizeT], const U (&actual)[sizeU])
-	{
-		return op(ProxyType<const T*>::create(expected).value,ProxyType<const U*>::create(actual).value);
+	MatcherResult binary_op(const _OP& op, const T(&expected)[sizeT], const U(&actual)[sizeU]) {
+		return op(ProxyType<const T*>::create(expected).value, ProxyType<const U*>::create(actual).value);
 	}
 
 	struct is_null {
@@ -73,14 +66,14 @@ namespace gppUnit {
 	};
 
 	template <typename T, typename DERIVED>
-	struct value_matcher{
-		explicit value_matcher(const T& expected):expectedValue(expected){}
+	struct value_matcher {
+		explicit value_matcher(const T& expected): expectedValue(expected) {}
 
 		const DERIVED& derivedMatcher() const { return static_cast<const DERIVED&>(*this); }
 
 		template <typename _Act>
 		MatcherResult match(const _Act& actual) const {
-			return binary_op(derivedMatcher(),actual,expectedValue);
+			return binary_op(derivedMatcher(), actual, expectedValue);
 		}
 
 		//is_not_t<_Derived> operator!() const { return is_not(derivedMatcher()); }
@@ -90,18 +83,18 @@ namespace gppUnit {
 
 	template <typename T>
 	struct equal_to_trait_base {
-		equal_to_trait_base(const T& actual, const T& expected):actualValue(actual),expectedValue(expected){}
+		equal_to_trait_base(const T& actual, const T& expected): actualValue(actual), expectedValue(expected) {}
 
-		bool equals(){ return (actualValue==expectedValue); }
+		bool equals() { return (actualValue == expectedValue); }
 
 		const T& actualValue;
 		const T& expectedValue;
 	};
 	template <typename T>
 	struct equal_to_trait: equal_to_trait_base<T> {
-		equal_to_trait(const T& actual, const T& expected):equal_to_trait_base<T>(actual,expected){}
+		equal_to_trait(const T& actual, const T& expected): equal_to_trait_base<T>(actual, expected) {}
 
-		MatcherResult match(){
+		MatcherResult match() {
 			MatcherResult r;
 			r.result = equal_to_trait_base<T>::equals();
 			r.strm << "'" << equal_to_trait_base<T>::expectedValue << "'";
@@ -110,13 +103,13 @@ namespace gppUnit {
 	};
 	template <>
 	struct equal_to_trait<std::string>: equal_to_trait_base<std::string> {
-		equal_to_trait(const std::string& actual, const std::string& expected):equal_to_trait_base<std::string>(actual,expected),
-				aperture(100),
-				elipsis("..."),
-				ray(aperture-elipsis.size()),
-				segment(ray-elipsis.size()),
-				fixedColumn(aperture/2)
-			{}
+		equal_to_trait(const std::string& actual, const std::string& expected): equal_to_trait_base<std::string>(actual, expected),
+			aperture(100),
+			elipsis("..."),
+			ray(aperture - elipsis.size()),
+			segment(ray - elipsis.size()),
+			fixedColumn(aperture / 2)
+		{}
 		const size_t aperture;
 		const std::string elipsis;
 		const size_t ray;
@@ -128,107 +121,101 @@ namespace gppUnit {
 			size_t shortest;
 			size_t mismatch;
 		};
-		sizes getSizes(void){
+		sizes getSizes(void) {
 			sizes s;
-			s.shortest=actualValue.size();
-			size_t longest=expectedValue.size();
-			if (longest<s.shortest){
-				std::swap(s.shortest,longest);
+			s.shortest = actualValue.size();
+			size_t longest = expectedValue.size();
+			if(longest < s.shortest) {
+				std::swap(s.shortest, longest);
 			}
 
-			s.mismatch=s.shortest;
-			for(size_t i=0;i<s.shortest;++i){
-				if (actualValue[i]!=expectedValue[i]){
-					s.mismatch=i;
+			s.mismatch = s.shortest;
+			for(size_t i = 0; i < s.shortest; ++i) {
+				if(actualValue[i] != expectedValue[i]) {
+					s.mismatch = i;
 					break;
 				}
 			}
 			return s;
 		}
 		struct formatParameters {
-			formatParameters():mmType(SHORT),mismatch(0){}
+			formatParameters(): mmType(SHORT), mismatch(0) {}
 			StringMismatchFormat mmType;
 			size_t mismatch;
 		};
 		formatParameters getParams(bool result) {
 			formatParameters params;
-			if (!result) {
-				sizes s=getSizes();
-				if(s.shortest<=10){
-					params.mmType=SHORT;
-				} else if (s.mismatch<=ray){
-					params.mmType=MEDIUM;
-					params.mismatch=s.mismatch;
+			if(!result) {
+				sizes s = getSizes();
+				if(s.shortest <= 10) {
+					params.mmType = SHORT;
+				} else if(s.mismatch <= ray) {
+					params.mmType = MEDIUM;
+					params.mismatch = s.mismatch;
 				} else {
-					params.mmType=LONG;
-					params.mismatch=s.mismatch;
+					params.mmType = LONG;
+					params.mismatch = s.mismatch;
 				}
 			}
 			return params;
 		}
-		MatcherResult match(){
+		MatcherResult match() {
 			MatcherResult r;
 			r.result = equals();
 
-			formatDescription(r.strm,getParams(r.result));
+			formatDescription(r.strm, getParams(r.result));
 			return r;
 		}
-		void formatDescription(TableFormatter& strm,formatParameters params){
-			switch (params.mmType){
+		void formatDescription(TableFormatter& strm, formatParameters params) {
+			switch(params.mmType) {
 			case SHORT:
 				strm << "string" << tab << rayString(expectedValue);
 				break;
 			case MEDIUM:
 				strm << "string" << tab << rayString(expectedValue) << endl;
-				strm << "first mismatch" << tab << std::string(params.mismatch+1,' ') << '^' << endl;
-				strm << "index " << params.mismatch << tab << std::string(params.mismatch+1,'-') << '|' << endl;
+				strm << "first mismatch" << tab << std::string(params.mismatch + 1, ' ') << '^' << endl;
+				strm << "index " << params.mismatch << tab << std::string(params.mismatch + 1, '-') << '|' << endl;
 				strm << "actual" << tab << rayString(actualValue) << endl;
 				break;
 			case LONG:
-				strm << "string" << tab << segmentString(expectedValue,params.mismatch) << endl;
-				strm << "first mismatch" << tab << std::string(fixedColumn+1,' ') << '^' << endl;
-				strm << "index " << params.mismatch << tab << std::string(fixedColumn+1,'-') << '|' << endl;
-				strm << "actual" << tab << segmentString(actualValue,params.mismatch) << endl;
+				strm << "string" << tab << segmentString(expectedValue, params.mismatch) << endl;
+				strm << "first mismatch" << tab << std::string(fixedColumn + 1, ' ') << '^' << endl;
+				strm << "index " << params.mismatch << tab << std::string(fixedColumn + 1, '-') << '|' << endl;
+				strm << "actual" << tab << segmentString(actualValue, params.mismatch) << endl;
 				break;
 			}
 		}
-		std::string rayString(const std::string& str){
+		std::string rayString(const std::string& str) {
 			std::stringstream strm;
 			strm << "'";
-			if(str.size()<=aperture){
+			if(str.size() <= aperture) {
 				strm << str;
 			} else {
-				strm << str.substr(0,ray) << elipsis;
+				strm << str.substr(0, ray) << elipsis;
 			}
 			strm << "'";
 			return strm.str();
 		}
-		std::string segmentString(const std::string& str, size_t mismatch){
+		std::string segmentString(const std::string& str, size_t mismatch) {
 			std::stringstream strm;
-			size_t startColumn=mismatch-fixedColumn+elipsis.size();
-			strm << "'" << elipsis  << str.substr(startColumn,segment) << elipsis << "'";
+			size_t startColumn = mismatch - fixedColumn + elipsis.size();
+			strm << "'" << elipsis  << str.substr(startColumn, segment) << elipsis << "'";
 			return strm.str();
 		}
 	};
 
 	template <typename T>
-	struct equal_to_t: value_matcher<T,equal_to_t<T> > {
-		explicit equal_to_t(const T& expected):value_matcher<T,equal_to_t<T> >(expected){}
+	struct equal_to_t: value_matcher<T, equal_to_t<T> > {
+		explicit equal_to_t(const T& expected): value_matcher<T, equal_to_t<T> >(expected) {}
 
 		template<typename V>
-		MatcherResult operator()(const V& actual, const V& expected) const
-		{
-			equal_to_trait<V> helper(actual,expected);
+		MatcherResult operator()(const V& actual, const V& expected) const {
+			equal_to_trait<V> helper(actual, expected);
 			return helper.match();
-
-			//MatcherResult result;
-			//result.result=(actual==expected);
-			//result.strm << "'" << expected << "'";
-			//return result;
 		}
 	};
 	template <typename T>
-	equal_to_t<T> equal_to(const T& expected){ return equal_to_t<T>(expected); }
+	equal_to_t<T> equal_to(const T& expected) { return equal_to_t<T>(expected); }
 }
 
 #endif // MATCHERS_H_A1A948B8_4BB3_4E59_8A68_E4E99CC5EEC2
--- test/TestEqualTo.cpp
+++ test/TestEqualTo.cpp
@@ -8,56 +8,115 @@ namespace TestEqualTo{
 	using gppUnit::equal_to;
 	using gppUnit::MatcherResult;
 
-	class IntAndLong: public Auto::TestCase{
+	class EqualToHelper: public Auto::TestCase{
+	protected:
 		template<typename MATCHER, typename ACTUAL>
-		void That(const ACTUAL& actual, MATCHER m){
+		void That(const ACTUAL& actual, MATCHER m, const char* desc){
 			MatcherResult result=m.match(actual);
 			confirm.equals(true,result.result,"result is true");
-			confirm.equals("'1'\n",result.description(),"description is '1'");
+			confirm.equals(desc,result.description(),"description is '1'");
 		}
 		template<typename MATCHER, typename ACTUAL>
-		void MisMatch(const ACTUAL& actual, MATCHER m){
+		void MisMatch(const ACTUAL& actual, MATCHER m, const char* desc){
 			MatcherResult result=m.match(actual);
 			confirm.equals(false,result.result,"result is false");
-			confirm.equals("'1'\n",result.description(),"description is '1'");
+			confirm.equals(desc,result.description(),"description is '1'");
 		}
+	};
+	class IntAndLong: public EqualToHelper{
 		void test(){
 			int actual=1;
 			long expected=1;
-			That(actual,equal_to(expected));
+			That(actual,equal_to(expected),"'1'\n");
 
 			actual=2;
-			MisMatch(actual,equal_to(expected));
-			//expect.fail("First Failing Test");
+			MisMatch(actual,equal_to(expected),"'1'\n");
 		}
 	}GPPUNIT_INSTANCE;
-	class StringTest: public Auto::TestCase{
-		template<typename MATCHER, typename ACTUAL>
-		void That(const ACTUAL& actual, MATCHER m){
-			MatcherResult result=m.match(actual);
-			confirm.equals(true,result.result,"result is true");
-			confirm.equals("string '1'\n",result.description(),"description is '1'");
-		}
+	class StringTest: public EqualToHelper{
 		void test(void){
 			const char* actual="1";
 			char expected[100];
 			strcpy(expected,actual);
-			That(actual,equal_to(expected));
+			That(actual,equal_to(expected),"string '1'\n");
 		}
 	}GPPUNIT_INSTANCE;
-	class StringTestFails: public Auto::TestCase{
-		template<typename MATCHER, typename ACTUAL>
-		void MisMatch(const ACTUAL& actual, MATCHER m){
-			MatcherResult result=m.match(actual);
-			confirm.equals(false,result.result,"result is false");
-			confirm.equals("string '2'\n",result.description(),"description is '2'");
-		}
+	class StringTestFails: public EqualToHelper{
 		void test(void){
-			const char* actual="1";
+			const char* actual="2";
 			char expected[100];
 			strcpy(expected,actual);
-			expected[0]='2';
-			MisMatch(actual,equal_to(expected));
+			expected[0]='1';
+			MisMatch(actual,equal_to(expected),"string '1'\n");
 		}
 	}GPPUNIT_INSTANCE;
 }
+
+namespace StringMismatchTests {
+	using gppUnit::equal_to;
+	using TestEqualTo::EqualToHelper;
+
+	class TestStringDescriptionTypeSHORT2: public EqualToHelper{
+		void test(void){
+			const char* actual="1";
+			const char* sample="1234567890";
+			char expected[150];
+			strcpy(expected,sample);
+			for(int i=0;i<10;++i) { strcat(expected,sample); }
+
+			MisMatch(actual,equal_to(expected),"string '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567...'\n");
+		}
+	}GPPUNIT_INSTANCE;
+	class TestStringDescriptionTypeMEDIUM1: public EqualToHelper{
+		void test(void){
+			const char* actual="abcdefghijkl";
+			char expected[100];
+			strcpy(expected,actual);
+			expected[6]='h';
+			MisMatch(actual,equal_to(expected),
+				"string         'abcdefhhijkl'\n"
+				"first mismatch        ^\n"
+				"index 6        -------|\n"
+				"actual         'abcdefghijkl'\n"
+				);
+		}
+	}GPPUNIT_INSTANCE;
+	class TestStringDescriptionTypeMEDIUM2: public EqualToHelper{
+		void test(void){
+			const char* actual="abcdefghijkl";
+			char expected[130];
+			strcpy(expected,actual);
+			for(int i=0;i<9;++i) { strcat(expected,actual); }
+
+			MisMatch(actual,equal_to(expected),
+							//            1         2         3         4         5         6         7         8         9        10
+							//   1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+				"string         'abcdefghijklabcdefghijklabcdefghijklabcdefghijklabcdefghijklabcdefghijklabcdefghijklabcdefghijkla...'\n"
+				"first mismatch              ^\n"
+				"index 12       -------------|\n"
+				"actual         'abcdefghijkl'\n"
+				);
+		}
+	}GPPUNIT_INSTANCE;
+	class TestStringDescriptionTypeLONG: public EqualToHelper{
+		void test(void){
+			const char* sample="abcdefghijkl";
+			char actual[250];
+			char expected[250];
+			strcpy(actual,sample);
+			for(int i=0;i<19;++i) { strcat(actual,sample); }
+			strcpy(expected,actual);
+			expected[150]='x';
+
+			MisMatch(actual,equal_to(expected),
+							//            1         2         3         4         5         6         7         8         9        10
+							//   1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+				"string         '...hijklabcdefghijklabcdefghijklabcdefghijklabcdefxhijklabcdefghijklabcdefghijklabcdefghijklabcde...'\n"
+				"first mismatch                                                    ^\n"
+				"index 150      ---------------------------------------------------|\n"
+				"actual         '...hijklabcdefghijklabcdefghijklabcdefghijklabcdefghijklabcdefghijklabcdefghijklabcdefghijklabcde...'\n"
+				);
+		}
+	}GPPUNIT_INSTANCE;
+}
+
