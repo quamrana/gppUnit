f2f35a606e45b03a48a29c11c8c36d98fe0846b7
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-01-31 17:36:59 +0000

    Added license to files.

--- src/StreamNotification.h
+++ src/StreamNotification.h
@@ -1,3 +1,24 @@
+/*
+Copyright (c) 2011 Andrew Wall
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 #ifndef STREAMNOTIFICATION_H_9DE37245_C9F0_4B33_A0A5_25E32A5DECE9
 #define STREAMNOTIFICATION_H_9DE37245_C9F0_4B33_A0A5_25E32A5DECE9
 
