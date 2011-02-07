2c4e796ba4a0052970c4e6759d9423a9aed13822
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-02-07 17:03:05 +0000

    Improved production of coverage information.

--- all-gcov.bat
+++ all-gcov.bat
@@ -1,5 +1,5 @@
 @echo off
 rem all-gcov
-for %%d in (Matchers src TableFormatting) do for %%f IN (%%d\*.cpp) do call run-gcov.bat %%d %%f
+for %%d in (Matchers src src\djgpp TableFormatting) do for %%f IN (%%d\*.cpp) do call run-gcov.bat %%d %%f
 del .\gcov\*.gcov
 move *.gcov .\gcov
