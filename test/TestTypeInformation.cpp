f9d7cc5093d73d22f415f3af2835fd7af263811f
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-02-08 00:06:37 +0000

    Make some tests improve coverage.

--- test/TestTypeInformation.cpp
+++ test/TestTypeInformation.cpp
@@ -36,12 +36,17 @@ namespace TestTypeInformation{
 	using gppUnit::equals;
 
 	class ClassInNamespace{};
-	class Test: public Auto::TestCase{
+	class ActualNames: public Auto::TestCase{
 		void test(){
 			confirm.that(gppUnit::demangleTypeName(typeid(ClassInNamespace).name()),equals("TestTypeInformation::ClassInNamespace"));
 			confirm.that(gppUnit::demangleTypeName(typeid(ClassInEmptyNamespace).name()),equals("(anonymous namespace)::ClassInEmptyNamespace"));
 			confirm.that(gppUnit::demangleTypeName(typeid(ClassInGlobalNamespace).name()),equals("ClassInGlobalNamespace"));
 			confirm.that(gppUnit::demangleTypeName(typeid(int).name()),equals("int"));
+		}
+	}GPPUNIT_INSTANCE;
+	class ArbitraryNames: public Auto::TestCase{
+		void test(){
+			confirm.that(gppUnit::demangleTypeName("foo"),equals("foo"));
 		}
 	}GPPUNIT_INSTANCE;
 }
