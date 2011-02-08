b212fab9f8cf00764718a5094edf7b2b7bfb529e
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-02-08 13:30:14 +0000

    Added ends_with.

--- test/TestSubStringMatching.cpp
+++ test/TestSubStringMatching.cpp
@@ -2,8 +2,8 @@
 
 namespace gppUnit{
 	template <typename T>
-	struct begins_with_t: value_matcher<T, begins_with_t<T> > {
-		explicit begins_with_t(const T& expected): value_matcher<T, begins_with_t<T> >(expected) {}
+	struct starts_with_t: value_matcher<T, starts_with_t<T> > {
+		explicit starts_with_t(const T& expected): value_matcher<T, starts_with_t<T> >(expected) {}
 
 		MatcherResult operator()(const std::string& actual, const std::string& expected) const {
 			size_t exp_size=expected.size();
@@ -13,18 +13,45 @@ namespace gppUnit{
 		}
 	};
 	template <typename T>
-	begins_with_t<T> begins_with(const T& expected) { return begins_with_t<T>(expected); }
+	starts_with_t<T> starts_with(const T& expected) { return starts_with_t<T>(expected); }
+
+	template <typename T>
+	struct ends_with_t: value_matcher<T, ends_with_t<T> > {
+		explicit ends_with_t(const T& expected): value_matcher<T, ends_with_t<T> >(expected) {}
+
+		MatcherResult operator()(const std::string& actual, const std::string& expected) const {
+			size_t exp_size=expected.size();
+			size_t act_size=actual.size();
+			bool booleanResult=(exp_size>act_size) ? false : 0 == actual.compare(act_size-exp_size, exp_size, expected);
+
+			MatcherResult result(booleanResult);
+			result.strm << "a string ending with '" << expected << "'";
+			return result;
+		}
+	};
+	template <typename T>
+	ends_with_t<T> ends_with(const T& expected){ return ends_with_t<T>(expected); }
 
 }
 
 namespace TestSubStringMatching{
 	using Utilities::MatcherHelper;
-	using gppUnit::begins_with;
+	using gppUnit::starts_with;
+	using gppUnit::ends_with;
 
 	class BeginsWith: public MatcherHelper{
 		void test(){
-			That("foo",begins_with("f"),"a string starting with 'f'\n");
-			That("bar",!begins_with("f"),"not a string starting with 'f'\n");
+			That("foo",starts_with("f"),"a string starting with 'f'\n");
+			That("bar",!starts_with("f"),"not a string starting with 'f'\n");
+			That("bar",!starts_with("doof"),"not a string starting with 'doof'\n");
+		}
+	}GPPUNIT_INSTANCE;
+
+	class EndsWith: public MatcherHelper{
+		void test(){
+			That("foo",ends_with("o"),"a string ending with 'o'\n");
+			That("bar",!ends_with("o"),"not a string ending with 'o'\n");
+			That("bar",!ends_with("doof"),"not a string ending with 'doof'\n");
 		}
 	}GPPUNIT_INSTANCE;
 
