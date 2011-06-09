dbc2d373fca1e2d1111ec1fa5ecb1f1b7e24548c
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-06-09 13:41:57 +0000

    Move all-gcov.bat to use mingw(unix) and add to gcov.ignore

--- Scripts/all-gcov.bat
+++ Scripts/all-gcov.bat
@@ -1,3 +1,3 @@
 @echo off
 rem all-gcov
-for %%d in (Matchers src src\djgpp TableFormatting test) do for %%f IN (%%d\*.cpp) do call run-gcov.bat %%d %%f
+for %%d in (Matchers src src\unix TableFormatting test) do for %%f IN (%%d\*.cpp) do call run-gcov.bat %%d %%f
