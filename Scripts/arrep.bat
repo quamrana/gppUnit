4b1dba51dabec9a064ef1ae2139c5c1a3f2b47c7
admin <admin@c916a74a-8973-6a45-a396-a05bf732ec27>
2011-06-17 16:08:08 +0000

    Removed redundant batch files.

--- Scripts/arrep.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Replacement for ar
-SET ARTMP=%RANDOM%_%RANDOM%.CMD
-ECHO %* > %ARTMP%
-ar @%ARTMP%
-del %ARTMP%
--- Scripts/linkrep.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Replacement for gpp when linking
-SET LINKTMP=%RANDOM%_%RANDOM%.CMD
-ECHO %* > %LINKTMP%
-gpp @%LINKTMP%
-del %LINKTMP%
