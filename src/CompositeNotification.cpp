1fa7a1191241e103e5cdf7ce206b841ebc4a53ab
Andrew <Andrew@c916a74a-8973-6a45-a396-a05bf732ec27>
2012-12-27 22:08:34 +0000

    Found a way to use for_each in CompositeNotification

--- src/CompositeNotification.cpp
+++ src/CompositeNotification.cpp
@@ -21,37 +21,38 @@ THE SOFTWARE.
 */
 #include "CompositeNotification.h"
 
+#include <algorithm>
+#include <functional>
+
 namespace gppUnit {
 
 	CompositeNotification::CompositeNotification(): collection() {}
 
 	template<typename C, typename N, typename A>
-	void caller1(C& collection, void (N::*f)(const A&), const A* a) {
+	void caller1(C& collection, void (N::*f)(const A&), const A& a) {
 		for(typename C::iterator it = collection.begin(), end = collection.end(); it != end; ++it) {
-			((*it)->*f)(*a);
+			((*it)->*f)(a);
 		}
 	}
 	template<typename C, typename N>
 	void caller(C& collection, void (N::*f)()) {
-		for(typename C::iterator it = collection.begin(), end = collection.end(); it != end; ++it) {
-			((*it)->*f)();
-		}
+        std::for_each(collection.begin(),collection.end(),std::mem_fun(f));
 	}
 
 	void CompositeNotification::StartProject(const ProjectDescription& desc) {
-		caller1(collection, &Notification::StartProject, &desc);
+		caller1(collection, &Notification::StartProject, desc);
 	}
 	void CompositeNotification::StartClass(const ClassDescription& desc) {
-		caller1(collection, &Notification::StartClass, &desc);
+		caller1(collection, &Notification::StartClass, desc);
 	}
 	void CompositeNotification::StartMethod(const MethodDescription& desc) {
-		caller1(collection, &Notification::StartMethod, &desc);
+		caller1(collection, &Notification::StartMethod, desc);
 	}
 	void CompositeNotification::Result(const TestResult& result) {
-		caller1(collection, &Notification::Result, &result);
+		caller1(collection, &Notification::Result, result);
 	}
 	void CompositeNotification::Exception(const std::string& what) {
-		caller1(collection, &Notification::Exception, &what);
+		caller1(collection, &Notification::Exception, what);
 	}
 	void CompositeNotification::EndMethod() {
 		caller(collection, &Notification::EndMethod);
