59b2c49369e814a420d95c224488b3a4e4157aa7
Andrew <Andrew@c916a74a-8973-6a45-a396-a05bf732ec27>
2012-12-27 21:33:59 +0000

    Update design notes.

--- setmingwscons.bat
+++ setmingwscons.bat
@@ -1,3 +1,3 @@
 @echo off
-set PATH=C:\MINGW\BIN;C:\Python27\Scripts;C:\Python27;C:\WINDOWS\system32;C:\WINDOWS;.\Scripts
+set PATH="C:\Program Files\Cppcheck";C:\MINGW\BIN;C:\Python27\Scripts;C:\Python27;C:\WINDOWS\system32;C:\WINDOWS;.\Scripts
 set PATH=%PATH%;.\Scripts
