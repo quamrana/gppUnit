0115b2337384d0b946cbcc8e5c53e0c6bf3199ae
Andrew <Andrew@c916a74a-8973-6a45-a396-a05bf732ec27>
2012-12-31 16:58:51 +0000

    Moved ClassDocumentation into its own module.

---
+++ src/ClassDocumentation.cpp
@@ -0,0 +1,62 @@
+#include "ClassDocumentation.h"
+
+#include "ClassDescription.h"
+
+#include <sstream>
+#include <stack>
+
+namespace gppUnit{
+
+        void ClassDocumentation::StartClass(const ClassDescription& aClass) {
+            collect.push_back(aClass.name());
+        }
+
+        std::string ClassDocumentation::getFormattedDocumentation() const {
+            std::stringstream str;
+
+            Head head;
+            std::stack<Head> headstack;
+            std::string tail;
+
+            vecstr::const_iterator it=collect.begin();
+            vecstr::const_iterator end=collect.end();
+
+            for(;;){
+                if(tail.empty()){
+                    if (it==end){
+                        return str.str();
+                    } else {
+                        tail=*it++;
+                    }
+                }
+
+                for(;;){
+                    std::string::size_type jt=tail.find(head.head);
+                    if(jt==0){
+                        tail=tail.substr(head.head.size());
+                        break;
+                    } else {
+                        head=headstack.top();
+                        headstack.pop();
+                    }
+                }
+                
+                for(;;){
+                    std::string::size_type ct=tail.find("::");
+                    if(ct!=std::string::npos){
+                        std::string nexthead=tail.substr(0,ct);
+                        std::string tmphead=nexthead+"::";
+                        tail=tail.substr(tmphead.size());
+                        headstack.push(head);
+                        head.head+=tmphead;
+                        str << head.spacing << nexthead << std::endl;
+                        head.spacing+=' ';
+                    } else{
+                        break;
+                    }
+                }
+                str << head.spacing << tail << std::endl;
+                tail="";
+            }
+        }
+}
