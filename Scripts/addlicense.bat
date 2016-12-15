e5b9a5e9a6f51c4fe481422500f3ab5d37df0477
andreww <andreww@localhost>
2016-12-15 15:14:17 +0000

    Added checkedbuild.bat

--- Scripts/addlicense.bat
+++ Scripts/addlicense.bat
@@ -1,4 +1,4 @@
 @echo off
 rem
 AddLicense.py --version
-rem AddLicense.py -f License.txt src\*.cpp src\*.h Matchers\*.cpp Matchers\*.h TableFormatting\*.cpp TableFormatting\*.h
+AddLicense.py -f licence.txt src\*.cpp src\*.h Matchers\*.cpp Matchers\*.h TableFormatting\*.cpp TableFormatting\*.h
