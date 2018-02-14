1e6e454f465d62678a22e7d8354331755b396cde
andreww <andreww@localhost>
2018-02-14 10:17:39 +0000

    Removed redundant method. Upgraded projects to VS2017 Added more disabled warnings from system headers.

--- test/Win32/TestQuotesToParentheses.cpp
+++ test/Win32/TestQuotesToParentheses.cpp
@@ -32,8 +32,6 @@ namespace TestTypeInformation {
 	using gppUnit::equal_to;
 
 	class TestQuotesToParentheses: public Auto::TestCase {
-		template<typename T>
-		std::string demangleTypeName() {}
 		void test() {
 			std::string typicalName = "`anonymous namespace'::";
 			gppUnit::replaceQuotesWithParentheses(typicalName);
