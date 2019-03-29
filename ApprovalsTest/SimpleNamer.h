a180c329823faa11917d1385434d96817c8b4a86
andreww <andreww@localhost>
2019-03-29 11:34:59 +0000

    Update scripts for gcc.

--- ApprovalsTest/SimpleNamer.h
+++ ApprovalsTest/SimpleNamer.h
@@ -1,3 +1,24 @@
+/*
+Copyright (c) 2011 Andrew Wall
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 #pragma once
 
 #include "ApprovalNamer.h"
@@ -8,14 +29,14 @@ namespace gppUnit {
 		//template<typename T>
 		//explicit SimpleNamer(const T& base): SimpleNamer(gppUnit::demangleTypeName(typeid(base).name())) {}
 		explicit SimpleNamer(const std::string& fullClassName) { makeNames(fullClassName); }
-		std::string getApprovedFile() const { return approvedFileName; }
-		std::string getReceivedFile() const { return receivedFileName; }
+		std::string getApprovedFile() const override { return approvedFileName; }
+		std::string getReceivedFile() const override { return receivedFileName; }
 
 	private:
 		void makeNames(const std::string& fullClassName);
 		std::string approvedFileName;
 		std::string receivedFileName;
 
-		std::string sanitized(const std::string& name);
+		static std::string sanitized(const std::string& name);
 	};
 }
--- Scripts/reformat.bat
+++ Scripts/reformat.bat
@@ -1,3 +1,3 @@
 @echo off
 astyle3 --version
-astyle3 --options=astyle.options src\*.cpp src\*.h Matchers\*.cpp Matchers\*.h TableFormatting\*.cpp TableFormatting\*.h test\*.cpp test\*.h | find "Formatted"
+astyle3 --options=astyle.options src\*.cpp src\*.h Matchers\*.cpp Matchers\*.h TableFormatting\*.cpp TableFormatting\*.h ApprovalsTest\*.cpp ApprovalsTest\*.h test\*.cpp test\*.h | find "Formatted"
