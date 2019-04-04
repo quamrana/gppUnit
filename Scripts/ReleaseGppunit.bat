1a221b91de8c6ad542733329589ac6cc7b24d738
andreww <andreww@localhost>
2019-04-04 15:26:49 +0100

    Also use ar to combine the .a files.

--- Scripts/ReleaseGppunit.bat
+++ Scripts/ReleaseGppunit.bat
@@ -1,10 +1,40 @@
 @echo off
 @echo Release gppUnit
 
-xcopy Debug\gppUnit.lib* Released\vs2017\gppUnit1.5\lib /I /Q /Y
-xcopy Debug\*.pdb Released\vs2017\gppUnit1.5\lib /I /Q /Y
+echo Copy vs2017 files
+xcopy Debug\gppUnit.lib* Released\vs2017\gppUnit1.5\lib /I /Q /Y >nul 2>&1
+xcopy Debug\*.pdb Released\vs2017\gppUnit1.5\lib /EXCLUDE:exclude_test_pdb /I /Q /Y >nul 2>&1
 
-xcopy Matchers\*.h Released\vs2017\gppUnit1.5\include\Matchers /I /Q /Y
-xcopy ApprovalsTest\*.h Released\vs2017\gppUnit1.5\include\ApprovalsTest /I /Q /Y
-xcopy TableFormatting\*.h Released\vs2017\gppUnit1.5\include\TableFormatting /I /Q /Y
-xcopy src\*.h Released\vs2017\gppUnit1.5\include\gppUnit /I /Q /Y
+echo Copy header files
+xcopy Matchers\*.h Released\vs2017\gppUnit1.5\include\Matchers /I /Q /Y >nul 2>&1
+xcopy ApprovalsTest\*.h Released\vs2017\gppUnit1.5\include\ApprovalsTest /I /Q /Y >nul 2>&1
+xcopy TableFormatting\*.h Released\vs2017\gppUnit1.5\include\TableFormatting /I /Q /Y >nul 2>&1
+xcopy src\*.h Released\vs2017\gppUnit1.5\include\gppUnit /I /Q /Y >nul 2>&1
+
+echo copy .a files
+mkdir Released\mingw\temp >nul 2>&1
+mkdir Released\mingw\lib >nul 2>&1
+xcopy src\lib*.a Released\mingw\temp /I /Q /Y >nul 2>&1
+xcopy ApprovalsTest\lib*.a Released\mingw\temp /I /Q /Y >nul 2>&1
+xcopy Matchers\lib*.a Released\mingw\temp /I /Q /Y >nul 2>&1
+xcopy TableFormatting\lib*.a Released\mingw\temp /I /Q /Y >nul 2>&1
+
+echo ar new combined .a
+PUSHD Released\mingw\temp
+ar -x libgppUnit1.5.a
+ar -x libApprovalsTest.a
+ar -x libMatchers.a
+ar -x libTableFormatting.a
+del libgppUnit.a >nul 2>&1
+del libgppUnit.b >nul 2>&1
+ar rc libgppUnit.a *.o
+ranlib libgppUnit.a
+del *.o
+ren libgppUnit.a libgppUnit.b
+del *.a
+echo move new .a file
+ren libgppUnit.b libgppUnit.a
+move /Y libgppUnit.a ..\lib >nul 2>&1
+POPD
+
+rmdir Released\mingw\temp /Q /S
